00308157a3bd879b1664576c192e85c53c8cf7df
ianvandevondel <ian.van.de.vondel@telenet.be>
2022-03-31 14:34:27 +0200

    database

--- database/GiftTracker.sql
+++ database/GiftTracker.sql
@@ -2,28 +2,35 @@ drop database GiftTracker;
 CREATE DATABASE GiftTracker;
 
 USE GiftTracker;
-create table gebruiker(
-persoon_ID int not null unique AUTO_INCREMENT,
+create table user(
+persoon_id int not null unique AUTO_INCREMENT,
 firstnaam varchar(100) not null,
 achternaam varchar(100) not null,
 leeftijd int not null,
 geslacht varchar (50) not null,
 paswoord varchar (50) not null,
 IsAdmin boolean,
-primary key(persoon_ID));
+prefrencescheck int,
+giftcheck int,
+calendercheck int,
+primary key(persoon_id),
+foreign key(calendercheck) references calender(calender_id),
+foreign key(prefrencescheck) references prefrences(prefrences_id),
+foreign key(giftcheck) references gift(gift_id)
+);
 
 create table prefrences(
-prefrences_ID int not null unique auto_increment,
+prefrences_id int not null unique auto_increment,
 preferences_Name int not null,
-primary key(prefrences_ID));
+primary key(prefrences_id));
 
 create table gift(
-gift_ID int not null unique auto_increment,
+gift_id int not null unique auto_increment,
 gift_Name int not null,
 primary key(gift_id));
 
-create table Calender(
-calender_ID int not null auto_increment,
+create table calender(
+calender_ID int not null unique auto_increment,
 EvenementName varchar(100) not null,
 Calender_Date date
 );
