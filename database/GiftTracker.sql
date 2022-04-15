df8ebf616205f7ea0bee2be7f1e29a8dd1fb8c1a
Kenny Van Eynde <vaneynde.kenny@hotmail.com>
2022-04-15 14:23:26 +0200

    update 15/04

--- database/GiftTracker.sql
+++ database/GiftTracker.sql
@@ -8,11 +8,18 @@ gift_id int not null unique auto_increment,
 gift_Name int not null,
 primary key(gift_id));
 
-create table calender(
-calender_id int not null unique auto_increment,
-EvenementName varchar(100) not null,
-Calender_Date date
+create table calendar(
+calendar_id int not null unique auto_increment,
+title varchar(255) COLLATE utf8_bin not null,
+start datetime not null,
+end datetime DEFAULT NULL
 );
+
+ALTER TABLE calendar
+    ADD PRIMARY KEY (calendar_id);
+
+ALTER TABLE calendar
+    MODIFY calendar_id int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 
 create table user(
 user_id int not null unique auto_increment,
@@ -26,9 +33,9 @@ password varchar (255) not null,
 UserAdmin varchar (50) default 'user',
 prefrencescheck int,
 giftcheck int,
-calendercheck int,
+calendarcheck int,
 primary key(user_id),
-foreign key(calendercheck) references calender(calender_id),
+foreign key(calendarcheck) references calendar(calendar_id),
 foreign key(prefrencescheck) references prefrences(prefrences_id),
 foreign key(giftcheck) references gift(gift_id)
 );
