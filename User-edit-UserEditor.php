fc1139a7363964e3d123e1632b4b0c4dc9c65a4f
Kenny Van Eynde <vaneynde.kenny@hotmail.com>
2022-05-02 14:45:59 +0200

    update admin-userEditor

--- User-edit-UserEditor.php
+++ User-edit-UserEditor.php
@@ -3,26 +3,26 @@
 
     session_start();
 
-    if(isset($_POST['submit'])){
-        
-            $newFirstname = $_POST['edit-firstname'];
-            $newLastname = $_POST['edit-lastname'];
-            $newBirthdate = $_POST['edit-birthdate'];
-            $newEmail = $_POST['edit-email'];
+    if(isset($_POST['submit']))
+    {
+        $newFirstname = $_POST['edit-firstname'];
+        $newLastname = $_POST['edit-lastname'];
+        $newBirthdate = $_POST['edit-birthdate'];
+        $newEmail = $_POST['edit-email'];
 
-            if(!empty($newFirstname) && !empty($newLastname) && !empty($newBirthdate) && !empty($newEmail)){
-                $loggedInUser = $_SESSION['username'];
-                $sql = "UPDATE user SET firstname = '$newFirstname', lastname = '$newLastname', birthdate = '$newBirthdate', email = '$newEmail' WHERE username = '$loggedInUser'";
+        if(!empty($newFirstname) && !empty($newLastname) && !empty($newBirthdate) && !empty($newEmail)){
+            $loggedInUser = $_SESSION['username'];
+            $sql = "UPDATE user SET firstname = '$newFirstname', lastname = '$newLastname', birthdate = '$newBirthdate', email = '$newEmail' WHERE username = '$loggedInUser'";
 
-                $results = mysqli_query($conn, $sql);
+            $results = mysqli_query($conn, $sql);
 
-                header("Location: User-edit-user.php?message=<div class='alert alert-succes'>Updated!</div>");
-                exit;
+            header("Location: User-edit-user.php?message=<div class='alert alert-succes'>Updated!</div>");
+            exit;
 
-            }else{
-                header("Location: User-edit-user.php?message=<div class='alert alert-succes'>Not Updated!</div>");
-                exit;
-            }
+        }else{
+            header("Location: User-edit-user.php?message=<div class='alert alert-succes'>Not Updated!</div>");
+            exit;
+        }
     }
 
 ?>
