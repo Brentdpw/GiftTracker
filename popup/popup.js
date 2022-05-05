70eac0299e59a5d066d7e92f98f4a3ebaa26b6c3
Brent De Pauw <brentdpw@gmail.com>
2022-05-05 18:17:05 +0200

    Popup overhaul

--- popup/popup.js
+++ popup/popup.js
@@ -6,3 +6,22 @@ function closePopup() {
     document.getElementById('test').style.display = 'none';
 }
 
+var ElementButton = document.querySelector("#button");
+var Input = document.getElementById("TitleAct");
+var amountSucces = 0;
+var Tekstvak = document.getElementById("tekstvak");
+var gevonden = false;
+
+ElementButton.addEventListener("click", function(){
+    
+    var newp = document.createElement("P");
+    newp.id = "tekst";
+    var tekst = document.createTextNode(Input.value);
+    newp.append(tekst);
+    newp.classList.add("tekst");
+    Tekstvak.append(newp);
+    gevonden = true;
+
+    closePopup()
+    document.getElementById("myForm").reset();
+});
