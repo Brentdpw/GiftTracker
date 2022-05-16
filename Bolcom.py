0679b4be9aeb8ff9fc658776bc19ef80dd465c96
ianvandevondel <ian.van.de.vondel@telenet.be>
2022-05-16 13:05:40 +0200

    update

--- Bolcom.py
+++ Bolcom.py
@@ -69,7 +69,7 @@ for items in productPhoto[:9]:
 
 for n in range(5):
     #print(creatorList[n]+" "+titleList[n]+" "+priceList[n]+" "+deliveryList[n]+" "+buttonList[n]+" "+imgList[n])
-    print('<div class="product">', '<div class="verkoper">', creatorList[n] ,'</div>', '<div class="productnaam">',titleList[n] ,'</div>', '<div class="prijs">', priceList[n],'</div>', '<div class="delivery">', deliveryList[n],'</div>', '<div class="link">', buttonList[n] ,'</div>', '<img">',imgList[n] ,'</img>', '</div>')
+    print('<div class="product">', '<div class="verkoper">', creatorList[n] ,'</div>', '<div class="productnaam">',titleList[n] ,'</div>', '<div class="prijs">', priceList[n],'</div>', '<div class="delivery">', deliveryList[n],'</div>', '<div class="link">', buttonList[n] ,'</div>', '<img src=',imgList[n],'alt=""> </div>')
     print('<br>')
     print()
     n+1
