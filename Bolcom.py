e13819cefcac4c87270d22b56cb229826acc998f
Brent De Pauw <brentdpw@gmail.com>
2022-05-17 12:00:07 +0200

    comment divprint

--- Bolcom.py
+++ Bolcom.py
@@ -75,12 +75,12 @@ for items in productPhoto[:9]:
     #print()
     #n+1
 
-for n in range(5):
-    #print(creatorList[n]+" "+titleList[n]+" "+priceList[n]+" "+deliveryList[n]+" "+buttonList[n]+" "+imgList[n])
-    print('<div class="product">', '<div class="verkoper">', creatorList[n] ,'</div>', '<div class="productnaam">',titleList[n] ,'</div>', '<div class="prijs">', priceList[n],'</div>', '<div class="delivery">', deliveryList[n],'</div>', '<div class="link">', buttonList[n] ,'</div>', '<img src=',imgList[n],'alt=""> </div>')
-    print('<br>')
-    print()
-    n+1
+# for n in range(5):
+#     #print(creatorList[n]+" "+titleList[n]+" "+priceList[n]+" "+deliveryList[n]+" "+buttonList[n]+" "+imgList[n])
+#     print('<div class="product">', '<div class="verkoper">', creatorList[n] ,'</div>', '<div class="productnaam">',titleList[n] ,'</div>', '<div class="prijs">', priceList[n],'</div>', '<div class="delivery">', deliveryList[n],'</div>', '<div class="link">', buttonList[n] ,'</div>', '<img src=',imgList[n],'alt=""> </div>')
+#     print('<br>')
+#     print()
+#     n+1
 
 mydb = mysql.connector.connect(
   host="ID361990_GiftTracker.db.webhosting.be",
