c07640c1338eb8f9dce6a780139d41e223d7f620
David Heymans <david.heymans@icloud.com>
2022-05-22 15:51:34 +0200

    Bol visualisatie update

--- Buypage.php
+++ Buypage.php
@@ -235,7 +235,7 @@
         <?php
             if (isset($_GET['product'])) {
                 $result = $_GET['product'];
-                echo $result;
+                #echo $result;
             }
             else echo("U moet een product kiezen of terug gaan naar het hoofdmenu")
             
@@ -246,4 +246,69 @@
 
     </body>
 
+</html>
+
+<?php 
+$username = "ID361990_GiftTracker"; 
+$password = "gifttracker123"; 
+$database = "ID361990_GiftTracker"; 
+$mysqli = new mysqli("ID361990_GiftTracker.db.webhosting.be", $username, $password, $database); 
+# checken voor connection
+// if ($mysqli->connect_error) {
+//     die('Connect Error (' .
+//     $mysqli->connect_errno . ') '.
+//     $mysqli->connect_error);
+// }
+ 
+$sql = "SELECT zoekterm, seller, title, price, delivery, button, imgLink FROM gift WHERE zoekterm IN ('$result') ";
+$result = $mysqli->query($sql);
+$mysqli->close();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <meta charset="UTF-8">
+    <title>GiftTracker</title>
+    <link rel="stylesheet" href="./assets/css/bolVis.css">
+</head>
+ 
+<body>
+    <section>
+        <h1>Products</h1>
+        <table>
+            <tr>
+                <th>Seller</th>
+                <th>Title</th>
+                <th>Image</th>
+                <th>Price</th>
+                <th>Link</th>
+            </tr>
+
+            <?php
+                while($rows=$result->fetch_assoc())
+                {
+                    $seller = $rows['seller'];
+                    $title = $rows['title'];
+                    $image = $rows['imgLink'];
+                    $price = $rows['price'];
+                    $button = $rows['button'];
+             ?>
+            <tr>
+                <td><?php echo $seller;?></td>
+                <td><?php echo $title;?></td>
+                <td><?php echo '<img src=',$image,'>';?></td>
+                <td><?php echo $price?></td>
+                <td><?php echo '<a href="',$button,'">','<button>','Click here','</button','</a>';?></td>
+
+            </tr>
+            <?php
+                }
+             ?>
+        </table>
+    </section>
+</body>
+ 
 </html>
