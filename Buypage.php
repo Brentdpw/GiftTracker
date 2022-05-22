08e8e552e7071da8bcb53aa4ed310451ab61aed5
Kenny Van Eynde <vaneynde.kenny@hotmail.com>
2022-05-22 22:02:13 +0200

    update

--- Buypage.php
+++ Buypage.php
@@ -227,8 +227,7 @@
                 <label for="product">Hoofdtelefoon</label>  -->
                 <br>
 
-
-                <input type="submit" value="submit">
+                <input type="submit" value="Search">
             </form>
         </div> 
   
@@ -249,21 +248,14 @@
 </html>
 
 <?php 
-$username = "ID361990_GiftTracker"; 
-$password = "gifttracker123"; 
-$database = "ID361990_GiftTracker"; 
-$mysqli = new mysqli("ID361990_GiftTracker.db.webhosting.be", $username, $password, $database); 
-# checken voor connection
-// if ($mysqli->connect_error) {
-//     die('Connect Error (' .
-//     $mysqli->connect_errno . ') '.
-//     $mysqli->connect_error);
-// }
- 
-$sql = "SELECT zoekterm, seller, title, price, delivery, button, imgLink FROM gift WHERE zoekterm IN ('$result') ";
-$result = $mysqli->query($sql);
-$mysqli->close();
-
+    $username = "ID361990_GiftTracker"; 
+    $password = "gifttracker123"; 
+    $database = "ID361990_GiftTracker"; 
+    $mysqli = new mysqli("ID361990_GiftTracker.db.webhosting.be", $username, $password, $database); 
+    
+    $sql = "SELECT zoekterm, seller, title, price, delivery, button, imgLink FROM gift WHERE zoekterm IN ('$result') ";
+    $result = $mysqli->query($sql);
+    $mysqli->close();
 ?>
 
 <!DOCTYPE html>
@@ -274,41 +266,43 @@ $mysqli->close();
     <title>GiftTracker</title>
     <link rel="stylesheet" href="./assets/css/bolVis.css">
 </head>
- 
-<body>
-    <section>
-        <h1>Products</h1>
-        <table>
-            <tr>
-                <th>Seller</th>
-                <th>Title</th>
-                <th>Image</th>
-                <th>Price</th>
-                <th>Link</th>
-            </tr>
+    <body>
+        <section>
+            <h1>Products</h1>
+            <table>
+                <tr>
+                    <th>Seller</th>
+                    <th>Title</th>
+                    <th>Image</th>
+                    <th>Price</th>
+                    <th>Link</th>
+                </tr>
 
-            <?php
-                while($rows=$result->fetch_assoc())
-                {
-                    $seller = $rows['seller'];
-                    $title = $rows['title'];
-                    $image = $rows['imgLink'];
-                    $price = $rows['price'];
-                    $button = $rows['button'];
-             ?>
-            <tr>
-                <td><?php echo $seller;?></td>
-                <td><?php echo $title;?></td>
-                <td><?php echo '<img src=',$image,'>';?></td>
-                <td><?php echo $price?></td>
-                <td><?php echo '<a href="',$button,'">','<button>','Click here','</button','</a>';?></td>
+                <?php
+                    while($rows=$result->fetch_assoc())
+                    {
+                        $seller = $rows['seller'];
+                        $title = $rows['title'];
+                        $image = $rows['imgLink'];
+                        $price = $rows['price'];
+                        $button = $rows['button'];
+                ?>
+                <tr>
+                    <td><?php echo $seller;?></td>
+                    <td><?php echo $title;?></td>
+                    <td><?php echo '<img src=',$image,'>';?></td>
+                    <td><?php echo $price?></td>
+                    <td><?php echo '<a href="',$button,'">','<button>','Click here','</button','</a>';?></td>
 
-            </tr>
-            <?php
-                }
-             ?>
-        </table>
-    </section>
-</body>
- 
+                </tr>
+                <?php
+                    }
+                ?>
+            </table>
+        </section>
+
+        <br>
+        <br>
+
+    </body>
 </html>
